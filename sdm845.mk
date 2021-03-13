95da3c2b11986969b01c9c42579f495c70dd4597
doc HD <doc.divxm@gmail.com>
2021-03-14 00:46:02 +0300

    sdm845-common: Add SystemUI QS Info paths & perms

--- sdm845.mk
+++ sdm845.mk
@@ -148,7 +148,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.aicp-sdm845.rc
 
 # Context Hub
 PRODUCT_PACKAGES += \
